74bd8faefe9a1cfddfd471e61b546b683097b9fd
thomasO82 <thomas.oliver@ri7.fr>
2025-03-10 12:06:30 +0100

    dsdsd

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const token = "VOTRE TOKEN RECU PAR MAIL"
+const urlBase = "http://146.59.242.125:3009/"
+const addForm = document.querySelector('#addForm')
+
+async function getPromos() {
+   const response = await fetch(urlBase + "promos", {
+    method:"GET",
+    headers: {
+        Authorization : "Bearer " + token
+    }
+   })
+   const data = await response.json()
+   console.log(data);
+}
+
+
+addForm.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+    let data = {
+        name : document.querySelector('#name').value,
+        startDate : document.querySelector('#startDate').value,
+        endDate: document.querySelector('#endDate').value,
+        formationDescription : document.querySelector('#formationDescription').value,
+    }
+    data = JSON.stringify(data)
+    const response = await fetch(urlBase + "promos", {
+        method:"POST",
+        headers: {
+            Authorization : "Bearer " + token,
+            "Content-type": "Application/json"
+        },
+        body: data
+       })
+       getPromos()
+})
+
+getPromos()
